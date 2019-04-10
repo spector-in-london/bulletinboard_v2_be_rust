7103ac1c6e1ca28258790fe9e0e967c6730efac7
robertschaap <robert.schaap@mac.com>
2019-04-10 18:58:29 +0200

    Main: setup component

---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Main = styled.main`
+  background-color: #fafafa;
+  padding: 1em 2.5em;
+  flex: auto;
+
+  @media (max-width: 450px) {
+    padding: 1em;
+  }
+`;
+
+export default Main;
