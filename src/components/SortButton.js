02197385586269ca890923f1555091cb732d0f53
robertschaap <robert.schaap@mac.com>
2019-04-10 18:55:27 +0200

    SortButton: setup component

---
+++ src/components/SortButton.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import Button from './Button';
+
+const SortButton = styled(Button)`
+  margin-bottom: 2em;
+  width: 200px;
+`;
+
+export default SortButton;
