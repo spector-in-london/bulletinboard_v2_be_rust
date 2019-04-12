cbc280fed7ec376b09e890a47c532d00d02bc061
robertschaap <robert.schaap@mac.com>
2019-04-12 19:25:22 +0200

    WritePage: setup component

---
+++ src/components/WritePage.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+import SubmitButton from '../components/SubmitButton';
+import InputField from '../components/InputField';
+import Message from '../components/Message';
+import TextareaField from '../components/TextareaField';
+
+class WritePage extends Component {
+  render() {
+    const { onChange, onSubmit, isBusy, hasError } = this.props;
+
+    return (
+      <form onSubmit={onSubmit}>
+        <InputField
+          autofocus
+          label='Your Name'
+          name='title'
+          onChange={onChange} />
+        <TextareaField
+          label='Your Comment'
+          name='body'
+          onChange={onChange} />
+        <SubmitButton
+          disabled={isBusy}
+          type='submit'>
+          Submit
+        </SubmitButton>
+        {hasError && <Message error/>}
+      </form>
+    );
+  }
+}
+
+export default WritePage;
