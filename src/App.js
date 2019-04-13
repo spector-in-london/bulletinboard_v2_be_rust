c6a7253f315f03b53daafe145807c22a0e9870f1
robertschaap <robert.schaap@mac.com>
2019-04-13 17:16:51 +0200

    App: setup component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import MainLayout from './components/MainLayout';
+import ReadPageContainer from './containers/ReadPageContainer';
+import WritePageContainer from './containers/WritePageContainer';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        Bulletinboard React & Rust
-      </div>
+      <BrowserRouter>
+        <MainLayout>
+          <Route exact path='/' component={ReadPageContainer} />
+          <Route path='/read' component={ReadPageContainer} />
+          <Route path='/write' component={WritePageContainer} />
+        </MainLayout>
+      </BrowserRouter>
     );
   }
 }
