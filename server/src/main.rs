32589da0406652b433678452bd5431dc4034b894
robertschaap <robert.schaap@mac.com>
2019-04-29 22:11:22 +0200

    server: setup basic router

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,20 @@
+extern crate iron;
+extern crate router;
+
+use iron::prelude::*;
+use iron::status;
+use router::Router;
+
+fn getCommentsHandler(_: &mut Request) -> IronResult<Response> {
+    Ok(Response::with((status::Ok, "response")))
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut router = Router::new();
+    router.get("/api/comments", getCommentsHandler, "index");
+
+    let address = "localhost:3001";
+    let _server = Iron::new(router).http(address).unwrap();
+
+    println!("Running on http://{}", address);
 }
