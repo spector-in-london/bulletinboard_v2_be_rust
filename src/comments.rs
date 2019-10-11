46521f499ca54bff35837e21198811a3c514adaa
robertschaap <robert.schaap@mac.com>
2019-10-11 21:35:36 +0200

    comments: work error logic into get comments handler

--- src/comments.rs
+++ src/comments.rs
@@ -16,28 +16,26 @@ struct Comments {
     comments: Vec<Comment>,
 }
 
-fn get_comments() -> Comments {
-    Comments{
+fn get_comments() -> Result<Comments> {
+    let comments = Comments{
         comments: vec![
             Comment { id: 1, title: "title1".to_string(), name: "name1".to_string(), body: "body1".to_string() },
             Comment { id: 2, title: "title2".to_string(), name: "name2".to_string(), body: "body2".to_string() },
             Comment { id: 3, title: "title3".to_string(), name: "name3".to_string(), body: "body3".to_string() },
             Comment { id: 4, title: "title4".to_string(), name: "name4".to_string(), body: "body4".to_string() },
         ],
-    }
+    };
+
+    Ok(comments)
 }
 
 pub fn get_comments_handler() -> Result<HttpResponse> {
-    let data = get_comments();
+    let response = match get_comments() {
+        Ok(data) => api::ApiResponse::success(data),
+        Err(_err) => api::ApiResponse::error(),
+    };
 
-    // TODO: implement properly
-    if true {
-        let response = api::ApiResponse::success(data);
-        Ok(HttpResponse::Ok().json(response))
-    } else {
-        let response = api::ApiResponse::<()>::error();
-        Ok(HttpResponse::Ok().json(response))
-    }
+    Ok(HttpResponse::Ok().json(response))
 }
 
 #[derive(Serialize, Deserialize)]
