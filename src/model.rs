bde58e76ccd1ca3f614364fad96c895d5bcafde1
robertschaap <robert.schaap@mac.com>
2019-11-02 15:21:02 +0100

    model: setup file and add get_comments method

---
+++ src/model.rs
@@ -0,0 +1,36 @@
+extern crate postgres;
+
+use postgres::{Connection, TlsMode};
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+pub struct Comment {
+    id: i32,
+    title: String,
+    name: String,
+    avatar: String,
+    body: String,
+}
+
+pub fn get_comments() -> Vec<Comment> {
+    let sql = "SELECT * FROM posts";
+
+    let conn = match Connection::connect("postgres://robertschaap@localhost/bulletinboard", TlsMode::None) {
+        Ok(r) => r,
+        Err(_) => return Vec::new(),
+    };
+
+    let mut comments: Vec<Comment> = Vec::new();
+
+    for row in &conn.query(sql, &[]).unwrap() {
+        comments.push(Comment {
+            id: row.get("id"),
+            title: row.get("title"),
+            body: row.get("body"),
+            avatar: row.get("avatar"),
+            name: row.get("name"),
+        });
+    }
+
+    return comments;
+}
